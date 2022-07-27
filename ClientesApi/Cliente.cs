14a2d57497712cc69e59f7752e8922d1ee1a4410
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2022-07-26 22:14:55 -0300

    feat: add clientes api

---
+++ ClientesApi/Cliente.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+
+namespace ClientesApi
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? CPF { get; set; }
+
+        public string? RG { get; set; }
+
+        public string? Telefone { get; set; }
+
+        public string? Endereco { get; set; }
+
+
+
+        public List<Cliente> listarCliente()
+        {
+            var filePath = Path.GetFullPath(@".\Data\Database.json");
+
+            var json = File.ReadAllText(filePath);   
+
+            var clients = JsonConvert.DeserializeObject<List<Cliente>>(json);
+
+            return clients;
+        }
+
+        public bool ReescreverArquivo(List<Cliente> clients)
+        {
+            var filePath = Path.GetFullPath(@".\Data\Database.json");
+
+            var json = JsonConvert.SerializeObject(clients, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+
+            return true;
+        }
+
+        public Cliente Inserir(Cliente Cliente)
+        {
+            var listaClientes = this.listarCliente();
+
+            var maxId = listaClientes.Max(cliente => cliente.Id);
+            Cliente.Id = maxId + 1;
+            listaClientes.Add(Cliente);
+
+            ReescreverArquivo(listaClientes);
+            return Cliente;
+        }
+
+        public Cliente Atualizar(int id, Cliente Cliente)
+        {
+            var listaClientes = this.listarCliente();
+
+            var itemIndex = listaClientes.FindIndex(cliente => cliente.Id == id);
+            if (itemIndex >= 0)
+            {
+                Cliente.Id = id;
+                listaClientes[itemIndex] = Cliente;
+            }
+            else
+            {
+                return null;
+            }
+
+            ReescreverArquivo(listaClientes);
+            return Cliente;
+        }
+
+        public bool Deletar(int id)
+        {
+            var listaClientes = this.listarCliente();
+
+            var itemIndex = listaClientes.FindIndex(cliente => cliente.Id == id);
+            if (itemIndex >=0 )
+            {
+                listaClientes.RemoveAt(itemIndex);
+            }
+            else
+            {
+                return false;
+            }
+
+            ReescreverArquivo(listaClientes);
+            return true;
+        }
+    }
+}
---
+++ ClientesApi/Controllers/ClienteController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+
+        [HttpGet("listar")]
+        public async Task<ActionResult<List<Cliente>>> Get()
+        {   
+            Cliente cliente = new Cliente();
+
+            return Ok(cliente.listarCliente());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cliente>>> GetByName([FromQuery] Cliente parameters)
+        {
+            Cliente cliente = new Cliente();
+
+            var getClientByName = cliente.listarCliente().Where(
+                (p) => string.Equals(p.Nome, parameters.Nome,
+                StringComparison.OrdinalIgnoreCase) || string.Equals(p.CPF, parameters.CPF,
+                StringComparison.OrdinalIgnoreCase)
+                );
+
+            if (getClientByName == null)
+                return BadRequest("Cliente não encontrado.");
+
+            return Ok(getClientByName);
+        }
+
+        [HttpPost("criar")]
+        public async Task<ActionResult<List<Cliente>>> AddClient(Cliente client)
+        {
+            Cliente _cliente = new Cliente();
+            _cliente.Inserir(client);
+        
+            return Ok(_cliente.listarCliente());
+        }
+
+        [HttpPut("atualizar/{id}")]
+        public async Task<ActionResult<List<Cliente>>> UpdateClient(Cliente cliente)
+        {
+            Cliente _cliente = new Cliente();
+            var getClientById = _cliente.listarCliente().Find(client => client.Id == cliente.Id);
+
+            if (getClientById == null)
+                return BadRequest("Cliente não encontrado.");
+
+            return Ok(_cliente.Atualizar(cliente.Id, cliente));
+        }
+
+        [HttpDelete("remover/{id}")]
+        public async Task<ActionResult<List<Cliente>>> DeleteClient(int id)
+        {
+            Cliente _cliente = new Cliente();
+            var getClientById = _cliente.listarCliente().Find(cliente => cliente.Id == id);
+
+            if (getClientById == null)
+                return BadRequest("Cliente não encontrado.");
+
+            return Ok(_cliente.Deletar(id));
+        }
+    }
+}
